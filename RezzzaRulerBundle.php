a909bac7135a5694d473a7d925e72cfe695c9665
Stéphane PY <py.stephane1@gmail.com>
2012-10-17 17:32:10 +0200

    Oops, i forgot that's a bundle :o

---
+++ RezzzaRulerBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rezzza\RulerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * RezzzaRulerBundle
+ *
+ * @uses Bundle
+ * @author Stephane PY <py.stephane1@gmail.com>
+ */
+class RezzzaRulerBundle extends Bundle
+{
+}
